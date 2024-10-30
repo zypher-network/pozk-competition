c6894d0b3a34b3fc5f85b1b002bd255493868468
Neo Sun <huachuang20@gmail.com>
2024-10-31 12:19:48 +1300

    Add basic zkvm test

---
+++ zkvm/methods/guest/src/main.rs
@@ -0,0 +1,19 @@
+use risc0_zkvm::guest::env;
+use sha2::{Sha256, Digest};
+use sha3::Keccak256;
+
+fn main() {
+    // read the input
+    let input: [u8; 32] = env::read();
+
+    let mut hasher = Sha256::new();
+    hasher.update(&input);
+    let result = hasher.finalize();
+
+    let mut hasher2 = Keccak256::new();
+    hasher2.update(&result);
+    let last = hasher2.finalize().to_vec();
+
+    // write public output to the journal
+    env::commit(&last);
+}
